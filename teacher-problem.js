010e24c0f10995e5cbd65a316f0d2a77c44e0b36
zpoettker <poettkerzach@gmail.com>
2024-07-26 11:24:30 -0500

    Create teacher-problem.js

---
+++ teacher-problem.js
@@ -0,0 +1,11 @@
+function getAverage(scores) {
+  let sum = 0
+  for(let i = 0; i < scores.length; i++){
+ sum = sum + scores[i];
+  }
+  return sum/scores.length;
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
