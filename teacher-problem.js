3b2ad799501abe56eca49931c3fd1854e4b30ae8
zpoettker <poettkerzach@gmail.com>
2024-07-26 11:32:17 -0500

    Update teacher-problem.js

--- teacher-problem.js
+++ teacher-problem.js
@@ -1,3 +1,6 @@
+//Calculate class average
+
+
 function getAverage(scores) {
   let sum = 0
   for(let i = 0; i < scores.length; i++){
@@ -9,3 +12,37 @@ function getAverage(scores) {
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+//get Grades
+
+
+
+function getGrade(score) {
+  if(score == 100){
+    return "A++";
+  }
+  else if(score >= 90 && score <= 99){
+    return "A";
+  }
+  else if(score >= 80 && score <= 89){
+    return "B";
+  }
+  else if(score >= 70 && score <= 79){
+    return "C";
+  }
+  else if(score >= 60 && score <= 69){
+    return "D";
+  }
+   else if(score >= 0 && score <= 59){
+     return "F";
+   }
+  else{
+    return "This is not a valid grade";
+  }
+
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
